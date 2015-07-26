833a78085c680e92381d56c31349a3981c8c7318
Martin Nowak <code@dawg.eu>
2015-07-26 06:51:17 +0200

    fix Issue 14801 – OS X installer not compatible with OS X 10.11
    - create symlinks in /usr/local/bin instead of /usr/bin
      to comply with new security policies

--- osx/post-install.sh
+++ osx/post-install.sh
@@ -6,13 +6,13 @@
 # License:: Public Domain
 #
 
-ln -sf /usr/share/dmd/bin/ddemangle /usr/bin/ddemangle
-ln -sf /usr/share/dmd/bin/dmd /usr/bin/dmd
-ln -sf /usr/share/dmd/bin/dmd.conf /usr/bin/dmd.conf
-ln -sf /usr/share/dmd/bin/dumpobj /usr/bin/dumpobj
-ln -sf /usr/share/dmd/bin/dustmite /usr/bin/dustmite
-ln -sf /usr/share/dmd/bin/obj2asm /usr/bin/obj2asm
-ln -sf /usr/share/dmd/bin/rdmd /usr/bin/rdmd
+ln -sf /usr/share/dmd/bin/ddemangle /usr/local/bin/ddemangle
+ln -sf /usr/share/dmd/bin/dmd /usr/local/bin/dmd
+ln -sf /usr/share/dmd/bin/dmd.conf /usr/local/bin/dmd.conf
+ln -sf /usr/share/dmd/bin/dumpobj /usr/local/bin/dumpobj
+ln -sf /usr/share/dmd/bin/dustmite /usr/local/bin/dustmite
+ln -sf /usr/share/dmd/bin/obj2asm /usr/local/bin/obj2asm
+ln -sf /usr/share/dmd/bin/rdmd /usr/local/bin/rdmd
 ln -sf /usr/share/dmd/man/man1/dmd.1 /usr/share/man/man1/dmd.1
 ln -sf /usr/share/dmd/man/man1/dumpobj.1 /usr/share/man/man1/dumpobj.1
 ln -sf /usr/share/dmd/man/man1/obj2asm.1 /usr/share/man/man1/obj2asm.1
